2828024df88e3192fe629b25c9094c5dc8ffcb34
ree-rishun <42071202+ree-rishun@users.noreply.github.com>
2020-09-21 20:55:53 +0900

    Firebaseキーの変更

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -3,24 +3,14 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import firebase from "firebase/app";
+import * as firebaseKey from "./firebase_key"
 
 Vue.config.productionTip = false;
 
 // Firebase設定
-const firebaseConfig = {
-  apiKey: "AIzaSyCbsJTh9uzknT8zv6QON6pBzR3VDf7iZnk",
-  authDomain: "chathub-656c4.firebaseapp.com",
-  databaseURL: "https://chathub-656c4.firebaseio.com",
-  projectId: "chathub-656c4",
-  storageBucket: "chathub-656c4.appspot.com",
-  messagingSenderId: "803132677884",
-  appId: "1:803132677884:web:4162c395236906c23d0179"
-};
-firebase.initializeApp(firebaseConfig);
-
+firebase.initializeApp(firebaseKey.firebaseConfig);
 window.firebase = firebase;
 
-/* eslint-disable no-new */
 new Vue({
   components: { App },
   router,
