2b7d6fa66ecca82283a029dd320c629c25fc0a91
ree-rishun <42071202+ree-rishun@users.noreply.github.com>
2020-09-21 20:58:31 +0900

    Firebaseキーの再編集

--- src/renderer/firebase_key.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCbsJTh9uzknT8zv6QON6pBzR3VDf7iZnk",
-    authDomain: "chathub-656c4.firebaseapp.com",
-    databaseURL: "https://chathub-656c4.firebaseio.com",
-    projectId: "chathub-656c4",
-    storageBucket: "chathub-656c4.appspot.com",
-    messagingSenderId: "803132677884",
-    appId: "1:803132677884:web:4162c395236906c23d0179"
-};
